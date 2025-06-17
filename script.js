56fc728e4f281b2d5dcdd211f597903e73db1b5d
Gib <brunorogerio.moretoni@gmail.com>
2025-06-16 23:08:33 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+document.querySelectorAll('#navbar a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetElement = document.querySelector(this.getAttribute('href'));
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
+
+
+
+const sections = document.querySelectorAll('section');
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+        }
+    });
+}, { threshold: 0.1 });
+
+sections.forEach(section => {
+    observer.observe(section);
+});
